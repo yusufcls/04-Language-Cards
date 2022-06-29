460701b6952aadde92a0bd5d26fc32283c26ea78
Yusuf C <yusufcfly@gmail.com>
2022-06-29 20:01:58 +0200

    Header.js-Card.js-App.css

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,1 +1,19 @@
-import React from 'react';
+import React from 'react'
+import { languages } from '../../helpers/data'
+import Item from '../item/Item'
+
+const Card = () => {
+  return (
+    <div className='cards-area-container'>
+        <div className='bars'></div>
+        <h1 className='language-title'>Languages</h1>
+        <div className='cards-container'>
+            {languages.map((item, index) => {
+                    return <Item card = {item} />
+                })};
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import reactLogo from '../../assets/react.svg';
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='logo-container'>
+       <img className='react-logo' src={reactLogo} alt="react-logo" />
+
+    </div>
+  )
+}
+
+export default Header
